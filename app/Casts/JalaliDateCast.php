a821afc0d1dd1bdb2e74d4d05f8fc0b73e2671b5
arcan5331 <alighaforian@yahoo.com>
2024-08-20 16:19:30 +0330

    fixed data being in the wrong format

--- app/Casts/JalaliDateCast.php
+++ app/Casts/JalaliDateCast.php
@@ -3,6 +3,7 @@
 namespace App\Casts;
 
 use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
+use Illuminate\Support\Carbon;
 use Morilog\Jalali\Jalalian;
 
 class JalaliDateCast implements CastsAttributes
@@ -10,24 +11,24 @@ class JalaliDateCast implements CastsAttributes
     /**
      * Cast the given value to Jalali date when retrieving from the database.
      *
-     * @param  \Illuminate\Database\Eloquent\Model  $model
-     * @param  string  $key
-     * @param  mixed  $value
-     * @param  array  $attributes
+     * @param \Illuminate\Database\Eloquent\Model $model
+     * @param string $key
+     * @param mixed $value
+     * @param array $attributes
      * @return string|null
      */
     public function get($model, string $key, $value, array $attributes): ?string
     {
-        return $value ? Jalalian::fromCarbon($value)->format('Y/m/d') : null;
+        return $value ? Jalalian::fromCarbon(Carbon::createFromFormat('Y-m-d', $value))->format('Y/m/d') : null;
     }
 
     /**
      * Prepare the given value for storage in the database as a Gregorian date.
      *
-     * @param  \Illuminate\Database\Eloquent\Model  $model
-     * @param  string  $key
-     * @param  mixed  $value
-     * @param  array  $attributes
+     * @param \Illuminate\Database\Eloquent\Model $model
+     * @param string $key
+     * @param mixed $value
+     * @param array $attributes
      * @return \Carbon\Carbon|null
      */
     public function set($model, string $key, $value, array $attributes): ?\Carbon\Carbon
