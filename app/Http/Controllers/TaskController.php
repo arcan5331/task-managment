37d0e29b461a2edc4e249db28d8635f0b6f6f46d
arcan5331 <alighaforian@yahoo.com>
2024-08-20 12:17:51 +0330

    added empty Controller

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function store(Request $request)
+    {
+
+    }
+
+    public function show(Request $request, Task $task)
+    {
+
+    }
+
+    public function update(Request $request, Task $task)
+    {
+
+    }
+
+    public function destroy(Task $task)
+    {
+
+    }
+}
