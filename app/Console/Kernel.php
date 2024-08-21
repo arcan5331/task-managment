164ce3cdc82fe95ac04c62ac5d0c09196ba2f168
arcan5331 <alighaforian@yahoo.com>
2024-08-21 12:28:44 +0330

    added a queue:work command to schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('queue:work --queue=high,medium,default,low --sleep=2 --tries=2 --backoff=1')
+            ->everyMinute()
+            ->withoutOverlapping();
     }
 
     /**
